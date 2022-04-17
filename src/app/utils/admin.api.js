5640ba7ad786ce82764b849a343b109ddc184bd6
vivek bhardwaj <vivek.bhardwaj@venturepact.com>
2022-04-17 18:12:52 +0530

    fix

--- src/app/utils/admin.api.js
+++ src/app/utils/admin.api.js
@@ -9,26 +9,4 @@ export const adminApi = {
 	login: (userData) => {
 		return axios.post(`http://localhost:8000/user/login`, userData);
 	},
-	// getProfile: () => {
-	// 	const authToken = localStorage.getItem("token");
-	// 	return axios.get(`http://localhost:8000/users/student/profile`,
-	// 	{headers: { authorization: `Bearer ${authToken}`}});
-	// },
 };
-
-
-// export const adminApi = {
-// 	register: (userData) => {
-// 		return fetch(`${environment.baseUrl}/users/student/register`, 
-// 		{method: 'Post',body: userData});
-// 	},
-// 	login: (userData) => {
-// 		return fetch(`${environment.baseUrl}/users/student/login`, 
-// 		{method: 'Post',body: userData});
-// 	},
-// 	getProfile: () => {
-// 		let userId = localStorage.getItem("userId");
-// 		return fetch(`${environment.baseUrl}/users/student/profile/${userId}`,
-// 		{method: 'GET',headers: { authorization: `Bearer ${authToken}`}});
-// 	},
-// };
